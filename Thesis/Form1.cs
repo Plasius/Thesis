2c3780a6ea1bf427c0bfd5e1e010a03525823578
Plasius <atiiworks@gmail.com>
2023-10-08 22:02:01 +0200

    Working auto-generated db and ERD

--- Thesis/Form1.cs
+++ Thesis/Form1.cs
@@ -7,23 +7,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Entity;
 
 namespace Thesis
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+
+            using (var db = new Model1())
+            {
+                var Customer = new Customer { CustomerID=1, companyName = "abc", address = "Address", taxNumber = "TaxNumber" };
+                db.customers.Add(Customer);
+
+                var Order = new Order { OrderID = 1 ,CustomerID = 1, payment_method = "Cash", orderDate = DateTime.Now, mentions = "Mentions", workID = "WorkID" };
+                db.orders.Add(Order);
+
+                var Core = new Core { CoreID = 1, OrderID = 1, CoreTypeID = 1, quantity = 1, length = 1, width = 1, thickness = 1, total_required_area = 1 };
+                db.cores.Add(Core);
+
+                var CoreType = new CoreType { CoreTypeID = 1 ,coreTypeName = "MDF" };
+                db.coreTypes.Add(CoreType);
+
+                var CorePrice = new CorePrice { CoreTypeID = 1, price = 1, date = DateTime.Now };
+                db.corePrices.Add(CorePrice);
+
+                
+
+                var VeneerType = new VeneerType { VeneerTypeID = 1, veneerTypeName = "Bukk" };
+                db.veneerTypes.Add(VeneerType);
+
+                var FaceVeneer = new Veneer { VeneerID = 1, CoreID = 1, VeneerTypeID = 1, parallelLength = 1, crossLength = 1, thickness = 1, isFaceVeneer = true };
+                db.veneers.Add(FaceVeneer);
+
+                var BackVeneer = new Veneer { VeneerID = 2, CoreID = 1, VeneerTypeID = 1, parallelLength = 1, crossLength = 1, thickness = 1, isFaceVeneer = false };
+                db.veneers.Add(BackVeneer);
+
+                var VeneerPrice = new VeneerPrice { VeneerTypeID = 1, price = 1, date = DateTime.Now};
+                db.veneerPrices.Add(VeneerPrice);
+
+                db.SaveChanges();
+
+                //display all customers from the database
+                var query = from b in db.customers
+                            orderby b.companyName
+                            select b;
+
+                // print all queried customers in a dialog box
+                string output = ""; 
+                foreach (var item in query)
+                {
+                    output += item.companyName + "\n";
+                }
+                MessageBox.Show(output);
+
+
+            }
         }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        //create fibonacci function with integers
-
 
     }
 }
---
+++ Thesis/Model1.cs
@@ -0,0 +1,129 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Thesis
+{
+    public class Model1 : DbContext
+    {
+        // Your context has been configured to use a 'Model1' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'Thesis.Model1' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'Model1' 
+        // connection string in the application configuration file.
+        public Model1()
+            : base("name=Model4")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        public virtual DbSet<Customer> customers { get; set; }
+        public virtual DbSet<Order> orders { get; set; }
+
+        public virtual DbSet<Core> cores { get; set; }
+
+        public virtual DbSet<Veneer> veneers { get; set; }
+
+        public virtual DbSet<CoreType> coreTypes { get; set; }
+
+        public virtual DbSet<VeneerType> veneerTypes { get; set; }
+
+        public virtual DbSet<CorePrice> corePrices { get; set; }
+
+        public virtual DbSet<VeneerPrice> veneerPrices { get; set; }
+    }
+
+    public class Customer
+    {
+        [Key]
+        public int CustomerID { get; set; }
+        public string companyName { get; set; }
+        public string address { get; set; }
+        public string taxNumber { get; set; }
+    }
+
+    public class Order {
+        [Key]
+        public int OrderID { get; set; }
+        public int CustomerID { get; set; }
+        public virtual Customer customer { get; set; }
+        public string payment_method { get; set; }
+        public DateTime orderDate { get; set; }
+        public string mentions { get; set; }
+        public string workID { get; set; }
+    }
+
+    public class Core {
+        [Key]
+        public int CoreID { get; set; }
+        public int OrderID { get; set; }
+        public virtual Order order { get; set; }
+        public int CoreTypeID { get; set; }
+        public virtual CoreType coreType { get; set; }
+        public int quantity { get; set; }
+        public double length { get; set; }
+        public double width { get; set; }
+        public double thickness { get; set; } 
+        public double total_required_area { get; set; }
+    
+    }
+
+    public class Veneer
+    {
+        [Key]
+        public int VeneerID { get; set; }
+        public int CoreID { get; set; }
+        public virtual Core core { get; set; }
+        public int VeneerTypeID { get; set; }
+        public virtual VeneerType veneerType { get; set; }
+        public int parallelLength { get; set; }
+        public double crossLength { get; set; }
+        public double thickness { get; set; }
+        public bool isFaceVeneer { get; set; }
+
+    }
+
+    public class CoreType
+    {
+        [Key]
+        public int CoreTypeID { get; set; }
+        public string coreTypeName { get; set; }
+
+    }
+
+    public class VeneerType
+    {
+        [Key]
+        public int VeneerTypeID { get; set; }
+        public string veneerTypeName { get; set; }
+
+    }
+
+
+    public class CorePrice
+    {
+        [Key]
+        public int CorePriceID { get; set; }
+        public int CoreTypeID { get; set; }
+        public virtual CoreType coreType { get; set; }
+        public double price { get; set; }
+        public DateTime date { get; set; }
+    }
+
+    public class VeneerPrice
+    {
+        [Key]
+        public int VeneerPriceID { get; set; }
+        public int VeneerTypeID { get; set; }
+        public virtual VeneerType veneerType { get; set; }
+        public double price { get; set; }
+        public DateTime date { get; set; }
+    }
+
+
+
+}
